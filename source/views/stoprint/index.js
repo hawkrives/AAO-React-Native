f24e39eae84dab15ce17738a970ba75b1710518c
Drew Volz <volz.drew@gmail.com>
2017-08-04 00:46:02 -0400

    blank view

---
+++ source/views/stoprint/index.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react'
+import {View} from 'react-native'
+
+export default class PrintReleaseView extends React.Component {
+  static navigationOptions = {
+    title: 'StoPrint',
+  }
+
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
